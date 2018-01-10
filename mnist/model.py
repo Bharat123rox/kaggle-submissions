dfc9130db5381ff578b5502f103cc29fe55acc77
Bhk <bharatraghunthan9767@gmail.com>
2018-01-10 09:09:38 +0530

    Small fix

--- mnist/model.py
+++ mnist/model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV 
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import svm
 
 labeled_images = pd.read_csv('train.csv')
